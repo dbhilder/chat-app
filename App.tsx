e01e3a100c210ab393c6ee0185c938036f7cf957
Derek Hilder <derek@oooh.tv>
2024-12-30 16:14:46 -0800

    Added initial implementation of amplify frontend app for chat demo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from "react";
+import { Button, View, StyleSheet, SafeAreaView } from "react-native";
+
+import { Amplify } from "aws-amplify";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+
+import outputs from "./amplify_outputs.json";
+
+import TodoList from "./src/TodoList";
+
+Amplify.configure(outputs);
+
+//import { Logger } from 'aws-amplify';
+//Logger.LOG_LEVEL = 'DEBUG'; // Set to 'INFO' or 'VERBOSE' for even more details
+
+const SignOutButton = () => {
+  const { signOut } = useAuthenticator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.signOutButton}>
+      <Button title="Sign Out" onPress={signOut} />
     </View>
   );
-}
+};
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+const App = () => {
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        <SafeAreaView style={styles.container}>
+          <SignOutButton />
+          {/* <StatusBar style="auto" /> */}
+          <TodoList />
+        </SafeAreaView>
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8,
+  },
+  signOutButton: {
+    alignSelf: "flex-end",
   },
 });
+
+export default App;
---
+++ aws_login.sh
@@ -0,0 +1,1 @@
+aws sso login --profile amplify-policy-982081084883
